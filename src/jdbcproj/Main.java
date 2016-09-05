0f4badd350806898e36176638387402b26f0c0af
Lebedev Alex <jkdsgjksdjg@gmail.com>
2016-09-05 15:19:15 +0400

    Class Person now is abstract. Added classes Student and Teacher which extend Person.

--- src/jdbcproj/Main.java
+++ src/jdbcproj/Main.java
@@ -2,7 +2,9 @@ package jdbcproj;
 
 import java.sql.SQLException;
 import java.util.List;
+import jdbcproj.crud.toperson.*;
 import jdbcproj.crud.togroup.*;
+import jdbcproj.person.Person;
 
 /**
  * This Class using for testing other classes.
@@ -12,6 +14,21 @@ import jdbcproj.crud.togroup.*;
  * */
 public class Main{
 	public static void main(String[] args){
+
+		CRUDPerson conn = new CRUDStudents();
+
+		try{
+
+			conn.add("test", "test");
+			List<Person> list = conn.getByName("test");
+
+			for(Person i: list){
+				System.out.println(i.getName() + " " + i.getFamilyName());
+			}
+
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
 
 	}
 }
